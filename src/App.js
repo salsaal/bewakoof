053bb9558997aada9aa8ba0405000165e5354fb0
Adnan <mdadnan0425@gmail.com>
2020-12-30 22:19:29 +0530

    3rd push

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ import Construction from "./layouts/pages/construction/construction";
 import { Switch, Route } from "react-router-dom";
 import men1 from "../src/assests/men1.jpg";
 import men2 from "../src/assests/men2.webp";
+import men3 from "../src/assests/men3.jpg";
+import men4 from "../src/assests/men4.jpg";
+import men5 from "../src/assests/men5.jpg";
+import men6 from "../src/assests/men6.jpg";
+import men7 from "../src/assests/men7.jpg";
+import men8 from "../src/assests/men8.jpg";
+import men9 from "../src/assests/men9.jpg";
+import men10 from "../src/assests/men10.jpg";
+
+
 
 export default class App extends React.Component {
   constructor(props) {
@@ -19,9 +29,9 @@ export default class App extends React.Component {
           image: men1,
           color: "black",
           for: "men",
-          name: 'Mens black',
+          type:"t-shirt",
+          name: 'Mens  t-shirt-black',
           price:'₹250',
-          type:"t-shirt"
         
         },
         {
@@ -29,9 +39,85 @@ export default class App extends React.Component {
           image: men2,
           color: "white",
           for: "men",
+          type:"t-shirt",
           name: 'Mens white',
           price:'₹240',
-          type:"t-shirt"
+        },
+        {
+          id: 3,
+          image: men3,
+          color: "black",
+          for: "men",
+          type:"t-shirt",
+          name: 'Mens black',
+          price:'₹250',
+        
+        },
+        {
+          id: 4,
+          image: men4,
+          color: "white",
+          for: "men",
+          type:"t-shirt",
+          name: 'Mens white',
+          price:'₹240',
+       
+        },
+        {
+          id: 5,
+          image: men5,
+          color: "black",
+          for: "men",
+          type:"t-shirt",
+          name: 'Mens black',
+          price:'₹250',
+        },
+        {
+          id: 6,
+          image: men6,
+          color: "white",
+          for: "men",
+          type:"t-shirt",
+          name: 'Mens white',
+          price:'₹240',
+        },
+        {
+          id: 7,
+          image: men7,
+          color: "black",
+          for: "men",
+          type:"t-shirt",
+          name: 'Mens black',
+          price:'₹250',
+        
+        },
+        {
+          id: 8,
+          image: men8,
+          color: "white",
+          for: "men",
+          type:"t-shirt",
+          name: 'Mens white',
+          price:'₹240',
+        },
+        {
+          id: 9,
+          image: men9,
+          color: "black",
+          for: "men",
+          type:"t-shirt",
+          name: 'Mens black',
+          price:'₹250',
+        
+        },
+        {
+          id: 10,
+          image: men10,
+          color: "white",
+          for: "men",
+          type:"t-shirt",
+          name: 'Mens white',
+          price:'₹240',
         },
       ],
     };
--- src/layouts/pages/construction/sections/card/card.jsx
+++ src/layouts/pages/construction/sections/card/card.jsx
@@ -4,9 +4,15 @@ export  default class Cardlist extends React.Component{
     render(props){
         return(
             <div className="card">
+                <div className="card-img-cony">
+                    <div className="flash"></div>
               <img src={this.props.products.image} alt=""/>
+              </div>
               <h5>{this.props.products.name}</h5>
               <h5>{this.props.products.price}</h5>
+              <div className="heart">
+              <i className="far fa-heart"></i>
+              </div>
             </div>
         )
     }
