709a624d05b7dedd69ff375c6a44192da4b9b20a
salsaal <salsaalshahid9038@gmail.com>
2021-01-20 22:10:32 +0530

    kwejf

--- src/layouts/pages/refer-earn/refer.jsx
+++ src/layouts/pages/refer-earn/refer.jsx
@@ -10,43 +10,55 @@ class Refer extends React.Component {
 
   render() {
     return (
-      <div className="refer">
-        <div className="refer-banner">
-          <div className="banner-text">
-            <h1>Refer a friend and earn ₹75</h1>
-            <p>
-              When they place an order using the referral code,
-              <br /> <span> they get credits worth ₹100</span>
-            </p>
-            <p className="banner-last-text">1 Bewakoof Credit = 1 Rupee</p>
+      <>
+        <div className="refer-separator"></div>
+        <div className="refer">
+          <div className="refer-banner">
+            <div className="banner-text">
+              <h1>Refer a friend and earn ₹75</h1>
+              <p>
+                When they place an order using the referral code,
+                <br /> <span> they get credits worth ₹100</span>
+              </p>
+              <p className="banner-last-text">1 Bewakoof Credit = 1 Rupee</p>
+            </div>
+            <img src={refer} alt="" />
           </div>
-          <img src={refer} alt="" />
-        </div>
-        <div className="refer-code">
-          <div className="code1">
-            <p>Copy the code below</p>
-            <span className="code">SAL6K5WG</span>
+          <div className="refer-code">
+            <div className="code1">
+              <p>Copy the code below</p>
+              <span className="code">SAL6K5WG</span>
+            </div>
+            <div className="code-separator"></div>
+            <div className="share-code">
+              <p>Share via</p>
+              <div className="code-options">
+                <a href="">
+                  <i class="fab fa-facebook"></i>
+                  facebook
+                </a>
+                <a href="">
+                  <i class="fab fa-twitter"></i>
+                  twitter
+                </a>
+                <a href="">
+                  <i className="fas fa-copy"></i>
+                  copy link
+                </a>
+              </div>
+            </div>
           </div>
-          <div className="code-separator"></div>
-          <div className="share-code">
-            <p>Share via</p>
-            <div className="code-options">
-              <a href="">
-              <i class="fab fa-facebook"></i>
-                facebook
-              </a>
-              <a href="">
-                <i className="fab fa-twitter"></i>
-                twitter
-              </a>
-              <a href="">
-                <i className="fas fa-copy"></i>
-                copy link
-              </a>
+          <div className="referal-work">
+            <p>How does referral work?</p>
+            <div className="work">
+              <p>Invite your friend to Bewakoof</p>
+              <p>They place their first order with referral code</p>
+              <p>Their order gets delivered</p>
+              <p>You get Bewakoof credits worth ₹75</p>
             </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
